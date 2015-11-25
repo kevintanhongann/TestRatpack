b040fe2c5321246c9ecb039757a60c723271bd9b
kevintan <kevintanhongann@gmail.com>
2015-11-25 19:23:34 +0800

    spring boot related application class. by right i dont need it yet

---
+++ src/main/groovy/com/testratpack/Application.groovy
@@ -0,0 +1,15 @@
+package com.testratpack
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by kevintan on 25/11/2015.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+class Application {
+
+}
